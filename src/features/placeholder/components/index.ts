576e56da8a61bbdc5e290fa6c80c4b317383b4f9
Dmitri Bostan <dm.bostan@gmail.com>
2023-11-11 09:00:09 +0100

    REFACTOR: Remove rendant components Placeholder and Logos

--- src/features/placeholder/components/index.ts
+++ src/features/placeholder/components/index.ts
@@ -2,4 +2,3 @@ export { default as Button } from './Button/Button.tsx';
 export { default as Heading } from './Heading/Heading.tsx';
 export { default as Link } from './Link/Link.tsx';
 export { default as Paragraph } from './Paragraph/Paragraph.tsx';
-export { default as Logos } from './Logos/Logos.tsx';
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './shared/styles/index.css';
-import { Placeholder } from '@pages/index.ts';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <Placeholder />
+      Hello, world!
   </React.StrictMode>,
 );
