7633e91ee0d8f1efca5c4f9902d35f945d8cbb86
Dmitri Bostan <dm.bostan@gmail.com>
2023-11-13 17:30:03 +0100

    WIP: Refactor ResultsContainer

--- src/features/search-results/ui/ResultsCounter/ResultsCounter.tsx
+++ src/features/search-results/ui/ResultsCounter/ResultsCounter.tsx
@@ -6,12 +6,12 @@ const ResultsCounter: React.FC<ResultsCounterType> = ({ totalFound, searchTerm }
 
   if(searchTerm) {
     return (
-        <h1 className={styles.component}>
+        <h1 className={styles.resultsCounter}>
           {totalFound} results found for {searchTerm}.
         </h1>
     );
   } else {
-    return (<h1>Please enter a search term. Maybe use "Statista" ?</h1>)
+    return (<h1 className={styles.resultsCounter}>Please enter a search term. Maybe use "Statista" ?</h1>)
   }
 
 
